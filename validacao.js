486e83215f3c35af2bd70ff53408b0aeb60fac54
gustavorocha-coder <gustavorocha9080@gmail.com>
2026-05-29 22:16:31 -0300

    exercicios validação
    atualização

---
+++ validacao.js
@@ -0,0 +1,20 @@
+function fnValidacao(){
+const campotituloFilme = document.getElementById("tituloFilme")
+
+if(campotituloFilme.value ==""){
+    return console.log('preencha o campo Titulo Filme')
+}
+console.log(campotituloFilme.value)
+
+
+const campoTituloOriginal = document.getElementById("TituloOriginal")
+if(campoTituloOriginal.value.length < 200){
+alert("voce digitou mais 5 numeros")
+return
+}
+
+const campoanoProducao = document.getElementById("anoProducao")
+if(campoanoProducao.value.)
+
+
+}
